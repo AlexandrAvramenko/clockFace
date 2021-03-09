4e829cbadaa0bbe4f24f9f0780efc5d12c6a541a
Alexandr Avramenko <alexandr_avramenko@MacBook-Pro-EU.local>
2021-03-09 22:54:45 +0200

    init

---
+++ dist/js/bundle.js
@@ -0,0 +1,98 @@
+//header menu
+// menu
+$(".header__burger").click(function () {
+  $(".header__menu-mobile").toggleClass("active");
+  $(".header__menu-small").toggleClass("hidden");
+  $(".header__logo-wrapper").toggleClass("hidden");
+  $(this).toggleClass("active");
+  // $("body").css("overflow-y","hidden")
+});
+
+//dropdown
+$('.search').click(function () {
+  $(this).toggleClass('active');
+  $('.search-content').slideToggle(300);
+  $('.overlay').fadeToggle(300);
+});
+
+$('.close__search-content').click(function () {
+  $('.search').remove('active');
+  $('.search-content').slideUp(300);
+  $('.overlay').hide();
+});
+
+//.header__menu-mobile
+$('.mobile-dropdown').click(function() {
+  $('.mobile-dropdown').removeClass('active');
+  if ($(this).next('.mobile-dropdown__content').css("display") == "none") {
+    $('.mobile-dropdown__content').slideUp(300);
+    $(this).next('.mobile-dropdown__content').slideToggle(300);
+    $('.mobile-dropdown').removeClass('active');
+    $(this).toggleClass('active');
+  } else $('.mobile-dropdown__content').slideUp(300);
+  return false;
+});
+
+//tabs
+$('[data-tab]').on('click', function () {
+  $(this).addClass('active').siblings('[data-tab]').removeClass('active')
+  $(this).siblings('[data-content=' + $(this).data('tab') + ']').addClass('active').siblings('[data-content]').removeClass('active')
+  $('.slider-best-deals').slick('refresh')
+})
+
+//sliders
+$(".slider-best-deals").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  prevArrow: "<img src='../img/arrow-left.svg' class='prev' alt='prev'>",
+  nextArrow: "<img src='../img/arrow-right.svg' class='next' alt='next'>",
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        dots: true
+      }
+    }
+  ]
+});
+
+$(".slider-reviews").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  prevArrow: "<img src='../img/arrow-left.svg' class='prev' alt='prev'>",
+  nextArrow: "<img src='../img/arrow-right.svg' class='next' alt='next'>",
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+// btn-up
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $(this).height()*0.5) {
+      $("#scrollup").fadeIn();
+    } else {
+      $("#scrollup").fadeOut();
+    }
+  });
+
+  $("#scrollup").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 800);
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+const 	gulp           = require('gulp'),
+        sass           = require('gulp-sass'),
+        cleanCSS       = require('gulp-clean-css'),
+        autoprefixer   = require('gulp-autoprefixer'),
+        removeComments = require('gulp-strip-css-comments'),
+        uglify         = require('gulp-uglify'),
+        browserSync    = require('browser-sync').create(),
+        sourcemaps 	   = require('gulp-sourcemaps'),
+        multipipe      = require('multipipe'),
+        rename         = require('gulp-rename'),
+        pug            = require('gulp-pug'),
+        imagemin       = require('gulp-imagemin'),
+        plumber        = require("gulp-plumber"),
+        cheerio        = require("gulp-cheerio"),
+        replace        = require("gulp-replace"),
+        rimraf         = require("rimraf"),
+        svgSprite      = require("gulp-svg-sprite");
+
+
+gulp.task('sass', function() {
+    return multipipe(
+        gulp.src('dev/sass/**/*.sass'),
+        (plumber()),
+        // (sourcemaps.init()),
+        (sass()),
+        (autoprefixer(['last 5 versions'])),
+        (gulp.dest('dist/css')),
+        (rename({suffix: '.min', prefix : ''})),
+        (cleanCSS()),
+        // (sourcemaps.write()),
+        (gulp.dest('dist/css')),
+        (browserSync.reload({stream: true})),
+    );
+});
+
+gulp.task('js', function() {
+    return multipipe(
+        gulp.src('dev/js/*.js'),
+        (plumber()),
+        (gulp.dest('dist/js')),
+        (rename({suffix: '.min', prefix : ''})),
+        (uglify()),
+        (removeComments()),
+        (gulp.dest('dist/js')),
+        (browserSync.reload({stream: true})),
+    );
+});
+
+gulp.task('image', function() {
+    return multipipe(
+        gulp.src('dev/img/**/*'),
+        (plumber()),
+        (imagemin([
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ])),
+        (gulp.dest('dist/img')),
+    );
+});
+
+gulp.task('sprite-svg', function() {
+    return multipipe(
+        gulp.src('dev/img/sprite/*.svg'),
+        (cheerio({
+        run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+        })),
+        (replace('&gt;', '>')),
+        (svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "../sprite.svg"
+                    }
+                },
+            }
+        )),
+        (gulp.dest('dist/img/')),
+    );
+});
+
+gulp.task('fonts', function() {
+    return multipipe(
+        gulp.src('dev/fonts/**/*.*'),
+        (gulp.dest('dist/fonts')),
+    );
+});
+
+gulp.task('gulp-pug', function gulpPug() {
+    return multipipe(
+        gulp.src('dev/pug/*.pug'),
+        (plumber()),
+        (pug({
+            pretty: true
+        })),
+        (gulp.dest('dist/'))
+    );
+});
+
+gulp.task('clean', function (cb) {
+    rimraf('dist', cb);
+});
+
+gulp.task('serve', function(done) {
+  browserSync.init({
+      server: "dist"
+  });
+
+  gulp.watch("dev/sass/**/*.sass", gulp.series('sass'));
+  gulp.watch('dev/pug/**/*.pug', gulp.series('gulp-pug'));
+  gulp.watch('dev/img/**/*.*', gulp.series('image'));
+  gulp.watch('dev/img/sprite.svg', gulp.series('sprite-svg'));
+  gulp.watch("dist/*.html").on('change', () => {
+    browserSync.reload();
+    done();
+  });
+  gulp.watch('dev/js/**/*.js', gulp.series('js'));
+
+
+  done();
+});
+
+gulp.task('default', gulp.series('clean', 'sass', 'js', 'image', 'sprite-svg', 'fonts', 'gulp-pug', 'serve'));
